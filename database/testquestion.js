e0898c87272fe28a7f4af723492efa26840a7c71
Taher Lunavadi <taherlunawadi@gmail.com>
2021-07-14 02:33:18 +0530

    Added DB and modified controllers and routes

--- database/testquestion.js
+++ database/testquestion.js
@@ -1,3 +1,25 @@
-module.exports = {
+const model = (require("../models/")).sequelize.models.TestQuestion
 
+class TestQuestionDB {
+  getAll = () => {
+    return model.findAll().then(data => data).catch(err => err)
+  }
+
+  getByID = (id) => {
+    return model.findByPk(id).then(data => data).catch(err => err)
+  }
+
+  add = (payload) => {
+    return model.create(payload).then(data => data).catch(err => err)
+  }
+
+  update = (id, payload) => {
+    return model.findByPk(id).then(data => data.update(payload).then(updatedData => updatedData).catch(err => err)).catch(err => err)
+  }
+
+  remove = (id) => {
+    return model.findByPk(id).then(data => data.destroy()).catch(err => err)
+  }
 }
+
+module.exports = TestQuestionDB
